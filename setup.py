5cb60533ce6ad92208cf11ae5d997961c2142d21
ben.maurer <ben.maurer@4da60275-9c2f-0410-b69a-795f42cc130f>
2007-05-29 05:01:04 +0000

    add classifiers/license
    git-svn-id: http://recaptcha.googlecode.com/svn/trunk/recaptcha-plugins/python@9 4da60275-9c2f-0410-b69a-795f42cc130f

--- setup.py
+++ setup.py
@@ -13,6 +13,15 @@ setup(name='recaptcha-client',
       description = "A plugin for reCAPTCHA and reCAPTCHA Mailhide",
       long_description = "Provides a CAPTCHA for Python using the reCAPTCHA service. Does not require any imaging libraries because the CAPTCHA is served directly from reCAPTCHA. Also allows you to securely obfuscate emails with Mailhide. This functionality requires pycrypto. This library requires two types of API keys. If you'd like to use the CAPTCHA, you'll need a key from http://recaptcha.net/api/getkey. For Mailhide, you'll need a key from http://mailhide.recaptcha.net/apikey",
 
+      license = "MIT/X11",
+      classifiers = [
+        "Topic :: Security",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        ],
+
+
       packages = [
         "recaptcha.client",
         ],
