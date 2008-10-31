38488921db04f5e8913098971bdd6d16ef232d1e
jabronson <jabronson@4da60275-9c2f-0410-b69a-795f42cc130f>
2008-10-31 05:13:14 +0000

    wrap long lines
    git-svn-id: http://recaptcha.googlecode.com/svn/trunk/recaptcha-plugins/python@107 4da60275-9c2f-0410-b69a-795f42cc130f

--- setup.py
+++ setup.py
@@ -7,11 +7,25 @@ from setuptools import setup, find_packages
 
 setup(name='recaptcha-client',
       version='1.0.3',
-      url = "http://recaptcha.net/",
+      url = "http://recaptcha.net",
       author = "Ben Maurer",
       author_email = "support@recaptcha.net",
       description = "A plugin for reCAPTCHA and reCAPTCHA Mailhide",
-      long_description = "Provides a CAPTCHA for Python using the reCAPTCHA service. Does not require any imaging libraries because the CAPTCHA is served directly from reCAPTCHA. Also allows you to securely obfuscate emails with Mailhide. This functionality requires pycrypto. This library requires two types of API keys. If you'd like to use the CAPTCHA, you'll need a key from http://recaptcha.net/api/getkey. For Mailhide, you'll need a key from http://mailhide.recaptcha.net/apikey",
+      long_description = """\
+Provides a CAPTCHA for Python using the reCAPTCHA service. Does not require
+any imaging libraries because the CAPTCHA is served directly from reCAPTCHA.
+Also allows you to securely obfuscate emails with Mailhide. This functionality
+requires pycrypto. This library requires two types of API keys. If you'd like
+to use the CAPTCHA, you'll need a key from http://recaptcha.net/api/getkey.
+For Mailhide, you'll need a key from http://mailhide.recaptcha.net/apikey.
+
+The trunk can be checked out from
+http://recaptcha.googlecode.com/svn/trunk/recaptcha-plugins/python,
+and the associated Google Code project lives at
+http://code.google.com/p/recaptcha.
+
+The package maintainer can be contacted at jabronson@gmail.com.
+""",
 
       license = "MIT/X11",
       classifiers = [
@@ -29,4 +43,3 @@ setup(name='recaptcha-client',
         },
       namespace_packages = ['recaptcha'],
       )
-
