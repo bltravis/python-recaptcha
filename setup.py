626d19e082fce92d3edb52b42433eecb6b1d7732
jabronson <jabronson@4da60275-9c2f-0410-b69a-795f42cc130f>
2009-07-30 19:28:03 +0000

    update long_description
    git-svn-id: http://recaptcha.googlecode.com/svn/trunk/recaptcha-plugins/python@119 4da60275-9c2f-0410-b69a-795f42cc130f

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='recaptcha-client',
       url = "http://recaptcha.net",
       author = "Ben Maurer",
       author_email = "support@recaptcha.net",
+      maintainer='Josh Bronson',
       description = "A plugin for reCAPTCHA and reCAPTCHA Mailhide",
       long_description = """\
 Provides a CAPTCHA for Python using the reCAPTCHA service. Does not require
@@ -22,9 +23,16 @@ For Mailhide, you'll need a key from http://mailhide.recaptcha.net/apikey.
 The trunk can be checked out from
 http://recaptcha.googlecode.com/svn/trunk/recaptcha-plugins/python,
 and the associated Google Code project lives at
-http://code.google.com/p/recaptcha.
+http://code.google.com/p/recaptcha. Note that this is a shared
+project/repository for reCAPTCHA clients for several languages/environments.
 
-The package maintainer can be contacted at jabronson@gmail.com.
+There is also a Google Group at http://groups.google.com/group/recaptcha/.
+Please use the associated mailing list for any questions or comments:
+recaptcha@googlegroups.com. Like the Google Code project, the Google Group
+mailing list is also shared among the several reCAPTCHA client implementations.
+
+reCAPTCHA is written by Ben Maurer and maintained by Josh Bronson. It is
+licensed under an MIT/X11 license.
 """,
 
       license = "MIT/X11",
